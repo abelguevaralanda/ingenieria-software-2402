37f33405a0cfc592f92fc2b37a90632396f5c683
Abel <abelguevaralanda@yahoo.es>
2024-04-14 17:21:31 +0200

    Add answer to Why use a stack structure and JavaScripts Arrays

--- exercises/data-structure/data-structure/src/DataStructureExercises/DataStructureExercise.ts
+++ exercises/data-structure/data-structure/src/DataStructureExercises/DataStructureExercise.ts
@@ -20,3 +20,9 @@ export const verificationSymbols = (symbols: string) => {
     return stack.length === 0;
 }
 
+/*
+Como estructura de datos, usaremos un stack (estructura de datos LIFO, Last In First Out) para almacenar los símbolos abiertos. Comprobando si los simbolos cerrados corresponden a los abiertos, se van eliminando los simbolos abiertos de la pila.
+
+En JavaScript, los Arrays son una estructura de datos dinámica que puede contener una colección ordenada de elementos, y pueden crecer o reducir su tamaño dinámicamente. Se implementan internamente utilizando una estructura similar a una lista vinculada, lo que les permite tener un acceso rápido a sus elementos mediante índices numéricos. Esto los hace adecuados para una amplia gama de aplicaciones, desde la implementación de pilas y colas hasta la gestión de datos estructurados y la realización de operaciones de búsqueda y filtrado.
+*/
+
