2ca95b59d9422d86729809af34a46bbd168c7595
Abel <abelguevaralanda@yahoo.es>
2024-04-14 17:08:32 +0200

    Add verifications test

--- exercises/data-structure/data-structure/src/DataStructureExercises/DataStructureExercise.test.ts
+++ exercises/data-structure/data-structure/src/DataStructureExercises/DataStructureExercise.test.ts
@@ -1,11 +1,32 @@
 
-import {describe, expect, test } from 'vitest'
+import { describe, expect, test, } from 'vitest'
 import {verificationSymbols} from "./DataStructureExercise.ts";
 
 describe('Given a function verificationSymbols', () => {
+
     describe('When it is called', () => {
         test('Then should it defined', () => {
             expect(verificationSymbols).toBeDefined()
         })
+
+        test('Then should receives a string', () => {
+            expect(verificationSymbols('')).toBe(true)
+        })
     });
+
+    describe.each`
+    symbols | expected
+    ${''} | ${true}
+    ${'()'} | ${true}
+    ${'[]'} | ${true}
+    ${'{}'} | ${true}
+    ${'([{}])'} | ${true}
+    ${'[({[]})]'} | ${true}
+    ${'({[]})]'} | ${false}
+    ${'[{[]})'} | ${false}
+    `('When it is called with $symbols = symbols couples string', ({ symbols, expected }) => {
+        test(`Then should return $expected = ${expected} result`, () => {
+            expect(verificationSymbols(symbols)).toBe(expected)
+        })
+    })
 });
