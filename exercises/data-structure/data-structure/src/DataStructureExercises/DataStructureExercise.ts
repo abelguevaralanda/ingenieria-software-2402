92778003a142b0a82aaf16f60908cb054be7a08b
Abel <abelguevaralanda@yahoo.es>
2024-04-14 17:10:24 +0200

    Add implementation to verify the symbols

--- exercises/data-structure/data-structure/src/DataStructureExercises/DataStructureExercise.ts
+++ exercises/data-structure/data-structure/src/DataStructureExercises/DataStructureExercise.ts
@@ -1,3 +1,22 @@
-export const verificationSymbols = (text: string) => {
-    return text;
+
+type SymbolsCouplesType = { [key: string]: string }
+export const verificationSymbols = (symbols: string) => {
+    const stack: string[] = [];
+    const symbolsCouples: SymbolsCouplesType = { '(': ')', '{': '}', '[': ']' };
+
+    for (const symbol of symbols) {
+        if (symbolsCouples[symbol]) {
+            stack.push(symbol);
+            continue;
+        }
+
+        const lastOpened = stack.pop();
+        const isValidClosing = lastOpened && symbolsCouples[lastOpened] === symbol;
+
+        if (!isValidClosing) {
+            return false;
+        }
+    }
+    return stack.length === 0;
 }
+
